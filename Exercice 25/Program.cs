a4676eb13264a94284e555238a9a7ffd9e7e9cc4
Vallignat <Vallignat@DESKTOP-CQH0LG7>
2022-10-18 14:23:48 +0200

    Ajoutez des fichiers projet.

---
+++ Exercice 25/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int vlr, vlre;
+            char rep='O';
+            bool txt=false;
+
+            Console.Write("Veuillez saisir plusieurs valeurs : ");
+            vlr = int.Parse(Console.ReadLine());
+            vlre = vlr;
+
+            do
+            {
+                Console.Write("Veuillez saisir plusieurs valeurs : ");
+                vlr = int.Parse(Console.ReadLine());
+                if (vlr > vlre)
+                {
+                    vlre = vlr;
+                    txt = true;
+                }
+                else
+                {
+                    txt = false;
+                }
+                Console.Write("Voulez vous continuer ? (O/N) : ");
+                rep = char.Parse(Console.ReadLine());
+            } while (rep != 'N');
+            
+            if (txt==true)
+            {
+                Console.WriteLine("La suite de valeurs saisies est strictement croissante.");
+            }
+            else
+            {
+                Console.WriteLine("La suite de valeurs saisies n'est pas croissante.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
